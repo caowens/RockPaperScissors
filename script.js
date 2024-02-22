338e7ac1811f93310656df5d07cc11b3420df1bf
caowens <caowens555@yahoo.com>
2024-02-22 14:59:05 -0600

    Finish MVP of game

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-/*
-Your game is going to play against the computer, 
-so begin with a function called getComputerChoice 
-that will randomly return either ‘Rock’, ‘Paper’ 
-or ‘Scissors’. We’ll use this function in the game 
-to make the computer’s play. Tip: use the console 
-to make sure this is returning the expected output 
-before moving to the next step!
-*/
 
+// Your game is going to play against the computer, 
+// so begin with a function called getComputerChoice 
+// that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 function getRandom(max) {
     return Math.floor(Math.random() * max);
 }
@@ -26,6 +20,7 @@ function getComputerChoice() {
     }
 }
 
+// Write a function that plays a single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection) {
   // your code here!
   playerSelection = playerSelection.toUpperCase();
@@ -54,11 +49,49 @@ function playRound(playerSelection, computerSelection) {
         return "You Lose! Scissors beats paper.";
     }
     else {
-        return "You win! Paper beats rock";
+        return "You Win! Paper beats rock";
     }
   }
 }
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+// Write a NEW function called playGame(). 
+// Use the previous function inside of this one 
+// to play a five round game that keeps score and 
+// reports a winner or loser at the end.
+function playGame() {
+    let playerScore = 0;
+    let computerScore  = 0;
+    playerSelections = ["Rock", "Paper", "Scissors", "Paper", "Rock"];
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = playerSelections[i];
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result.length > 4) {
+            console.log(result[4]);
+            if (result[4] === "L") {
+                computerScore++;
+            }
+            else {
+                playerScore++;
+            }
+        }
+    }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+
+    if (playerScore > computerScore) {
+        console.log("You win!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("You lose!");
+    }
+    else {
+        console.log("It's a tie!");
+    }
+
+
+}
+playGame();
